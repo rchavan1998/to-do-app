854857b12690d5f477c63d0988e437c0ac90dcc8
Rahul Chavan <rchavan@umich.edu>
2024-05-16 10:51:39 -0400

    add new task

--- to-do-app.py
+++ to-do-app.py
@@ -1,5 +1,6 @@
 import psycopg2
 from psycopg2 import sql
+from datetime import datetime
 
 
 #postgres init 
@@ -14,22 +15,44 @@ def view_all_tasks():
     for row in all_tasks_list:
         print(f"{row[0]}: {row[1]} - due by {row[2]}")
 
+def add_new_task(task, due_date):
+    if due_date == '':
+        due_date = 'not added'  
+    if due_date == 'not added':
+        due_date = datetime.now()
+    pg_cursor.execute(
+        "INSERT INTO tasks (task_name, due_date) VALUES (%s, %s)",
+        (task, due_date)
+    )
+    pg_conn_str.commit()
+    print("Task added successfully !")
 
 
 #MAIN FUNCTION
 def to_do_app():
         print("Select an option from 1-4:")
-        # print("1. Add a task")
+        print("1. Add a task")
         # print("2. Delete a task")
         print("3. View all tasks")
         # print("4. Update a task")
         # print("5. Exit to main menu")
         user_input = input("Enter your choice : ")
 
-        if user_input == '3':
+        if user_input == '1' :
+            task = input("Enter task details: ")
+            due_date = input("Enter due date (timestamp without timezone) (YYYY-MM-DD HH:MM:SS), or leave blank for current timestamp: ")
+            if due_date:
+                due_date = datetime.strptime(due_date, "%Y-%m-%d %H:%M:%S")
+            add_new_task(task, due_date)
+
+
+
+
+
+        elif user_input == '3':
              view_all_tasks()
         else:
-             print ("Enter only integer input. Try again")
+             print ("Enter only integer input between 1-4. Try again")
 
 
 if __name__ == "__main__":

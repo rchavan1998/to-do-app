4bf41334254cc33e3c1c100294754f3f6e49d3fc
Rahul Chavan <rchavan@umich.edu>
2024-05-16 11:04:26 -0400

    update

--- to-do-app.py
+++ to-do-app.py
@@ -36,13 +36,28 @@ def drop_task(task_id):
     pg_conn_str.commit()
     print("Task deleted!")
 
+#update
+def update_task(task_id, new_task_name=None, new_due_date=None):
+    if new_task_name:
+        pg_cursor.execute(
+            "UPDATE tasks SET task_name = %s WHERE id = %s",
+            (new_task_name, task_id)
+        )
+    if new_due_date:
+        pg_cursor.execute(
+            "UPDATE tasks SET due_date = %s WHERE id = %s",
+            (new_due_date, task_id)
+        )
+    pg_conn_str.commit()
+    print(f"Task with id: {task_id} updated successfully!")
+
 #MAIN FUNCTION
 def to_do_app():
         print("Select an option from 1-4:")
         print("1. Add a task")
         print("2. Delete a task")
         print("3. View all tasks")
-        #print("4. Update a task")
+        print("4. Update a task")
         # print("5. Exit to main menu")
         user_input = input("Enter your choice : ")
 
@@ -61,6 +76,17 @@ def to_do_app():
 
         elif user_input == '3':
              view_all_tasks()
+
+        elif user_input == '4' :
+
+            task_id = int(input("Enter task ID to update: "))
+            new_task = input("Enter new task details or leave blank: ")
+            new_due_date = input("Enter new due date (YYYY-MM-DD HH:MM:SS), or leave blank: ")
+
+            if new_due_date:
+                new_due_date = datetime.strptime(new_due_date, "%Y-%m-%d %H:%M:%S")
+            update_task(task_id, new_task, new_due_date)
+
         else:
              print ("Enter only integer input between 1-4. Try again")
 
